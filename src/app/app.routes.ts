25097144c2a4aa13ade04dfef43009efc9f87e6f
Erick Valdiviezo <erickvaldiviezo@Laptop-de-Erick.local>
2024-06-29 13:08:29 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
 
-export const routes: Routes = [];
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { OnboardingPageComponent } from './pages/onboarding-page/onboarding-page.component';
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/Home', pathMatch: 'full'},
+    { path: 'Home', component: HomePageComponent },
+    { path: 'About', component: AboutPageComponent },
+    { path: 'Aplication', component: OnboardingPageComponent },
+
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports:[RouterModule],
+})
+
+export class AppRoutingModule{}
